7986b1b67522064dd11e9c64f96de657a4037379
Kolesnikova-ekaterina <82054029+Kolesnikova-ekaterina@users.noreply.github.com>
2025-06-23 17:12:35 +0300

    Update CustomWebApplicationFactory.cs

--- AuctionApp.Tests/TestHelpers/CustomWebApplicationFactory.cs
+++ AuctionApp.Tests/TestHelpers/CustomWebApplicationFactory.cs
@@ -10,26 +10,26 @@ namespace AuctionApp.Tests.TestHelpers
 {
     public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
-        // Задайте здесь строку подключения к вашей тестовой базе SQL Server
+       
         private const string TestConnectionString = "Server=(localdb)\\mssqllocaldb;Database=AuctionAppTestDb;Trusted_Connection=True;MultipleActiveResultSets=true";
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services =>
             {
-                // Удаляем регистрацию DbContext, если есть
+            
                 var descriptor = services.SingleOrDefault(
                     d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
                 if (descriptor != null)
                     services.Remove(descriptor);
 
-                // Регистрируем DbContext с SQL Server и тестовой строкой подключения
+
                 services.AddDbContext<AppDbContext>(options =>
                 {
                     options.UseSqlServer(TestConnectionString);
                 });
 
-                // Создаем провайдер и инициализируем базу
+
                 var sp = services.BuildServiceProvider();
                 using var scope = sp.CreateScope();
                 var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
